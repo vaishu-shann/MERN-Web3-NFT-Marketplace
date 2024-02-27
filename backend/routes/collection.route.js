49b0d0cd1ba5a4aa1dd310ec780036a5f0e11a7b
vaishu-shann <ilukiabi@gmail.com>
2024-02-27 16:42:17 +0530

    Minor issue fix - Backend

--- backend/routes/collection.route.js
+++ backend/routes/collection.route.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getCollections,getCollectionsByUser,getFewCollections,createCollection} = require("../controllers/collection.controller");
+const { getCollections,getCollectionsByUser,getFewCollections,createCollection,getCollectionDetailsById} = require("../controllers/collection.controller");
 const router = express.Router();
 const{ Upload } =require( "../controllers/multer.controller");
 
@@ -7,5 +7,6 @@ router.post("/getCollections", getCollections);
 router.post("/getCollectionsByUser", getCollectionsByUser);
 router.post("/getFewCollections", getFewCollections);
 router.post("/addCollection", Upload.any("file"), createCollection);
+router.post("/getCollectionDetailsById", getCollectionDetailsById);
 
 module.exports = router;
